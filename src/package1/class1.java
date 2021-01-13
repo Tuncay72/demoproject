e9c200fbf0984c3569a3fb192bf32db601e8b18b
Tuncay72 <64120952+Tuncay72@users.noreply.github.com>
2021-01-13 17:35:29 -0600

    Update class1.java
    some update from local feature

--- src/package1/class1.java
+++ src/package1/class1.java
@@ -7,5 +7,7 @@ public class class1 {
         System.out.println("Hello word");
 
         System.out.println("Here are my update about demo class");
+        
+        //some update from local feature
     }
 }
